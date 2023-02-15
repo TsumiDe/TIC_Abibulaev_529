b3b329a3079bae3c9f4d5e1762310d9a5a9ea1b0
TsumiDe <m.m.abibulayev@student.khai.edu>
2023-02-15 22:23:40 +0100

    Signal processing, 2

--- SignalProcessing/figures/SignalProcessing.py
+++ SignalProcessing/figures/SignalProcessing.py
@@ -9,6 +9,8 @@ class Main:
         self.n = 500
         self.Fs = 1000
         self.FMax = 3
+        self.Dt = [2, 4, 8, 16]
+        self.FFilter = 10
 
     def main(self):
         randomNormal = numpy.random.normal(0, 10, self.n)
@@ -16,15 +18,59 @@ class Main:
         w = self.FMax / (self.Fs / 2)
         parametersFilter = signal.butter(3, w, "low", output="sos")
         y = signal.sosfiltfilt(parametersFilter, randomNormal)
-        self.plot(x, y, "Час (секунди)", "Амплітуда сигналу", "Сигнал")
-
-        spectrum = fft.fft(y)
-        spectrumCenter = numpy.abs(fft.fftshift(spectrum))
         frequencyReadings = fft.fftfreq(self.n, 1 / self.n)
         frequencyReadingsCenter = fft.fftshift(frequencyReadings)
-        self.plot(frequencyReadingsCenter, spectrumCenter, "Частота(Гц)", "Амплітуда спектру", "Спектр сигналу")
+        discreteSignals = []
+        discreteSpectrums = []
+        vars2 = []
+        SNRList = []
+        for Dt in self.Dt:
+            discreteSignal = numpy.zeros(self.n)
+            for i in range(0, round(self.n / Dt)):
+                discreteSignal[i * Dt] = y[i * Dt]
+            discreteSpectrum = fft.fft(discreteSignal)
+            discreteSpectrum = numpy.abs(fft.fftshift(discreteSpectrum))
+            discreteSignals.append(discreteSignal)
+            discreteSpectrums.append(discreteSpectrum)
+            E1 = discreteSignal - y
+            var1 = numpy.var(y)
+            var2 = numpy.var(E1)
+            vars2.append(var2)
+            SNR = var1 / var2
+            SNRList.append(SNR)
+        w = self.FFilter / (self.Fs / 2)
+        parametersFilter = signal.butter(3, w, 'low', output='sos')
+        y = signal.sosfiltfilt(parametersFilter, discreteSignals)
+        self.subplot(x, discreteSignals, "Час (секунди)", "Амплітуда сигналу",
+                     "Сигнал з кроком дисркетизації Dt = (2, 4, 8, 16)")
+        self.subplot(frequencyReadingsCenter, discreteSpectrums, "Частота(Гц)", "Амплітуда спектру",
+                     "Спектри сигналів з кроком дисркетизації Dt = (2, 4, 8, 16)")
+        self.subplot(x, y, "Час (секунди)", "Амплітуда сигналу",
+                     "Відновлені аналогові сигнали з кроком дисркетизації Dt = (2, 4, 8, 16)")
+        self.plot(self.Dt, vars2, "Крок дискретизації", "Дисперсія",
+                  "Залежність дисперсії від кроку дискретизації")
+        self.plot(self.Dt, SNRList, "Крок дискретизації", "ССШ",
+                  "Залежність співвідношення сигнал-шум від кроку дискретизації")
 
-    def plot(self, x, y, xLabel, yLabel, title):
+    @staticmethod
+    def subplot(x, y, xLabel, yLabel, title):
+        try:
+            fig, ax = plt.subplots(2, 2, figsize=(21 / 2.54, 14 / 2.54))
+            s = 0
+            for i in range(0, 2):
+                for j in range(0, 2):
+                    ax[i][j].plot(x, y[s], linewidth=1)
+                    s += 1
+            fig.supxlabel(xLabel, fontsize=14)
+            fig.supylabel(yLabel, fontsize=14)
+            fig.suptitle(title, fontsize=14)
+            fig.savefig(f"{title}.png", dpi=600)
+            plt.show()
+        except Exception as ex:
+            print(ex)
+
+    @staticmethod
+    def plot(x, y, xLabel, yLabel, title):
         try:
             fig, ax = plt.subplots(figsize=(21 / 2.54, 14 / 2.54))
             ax.plot(x, y, linewidth=1)
