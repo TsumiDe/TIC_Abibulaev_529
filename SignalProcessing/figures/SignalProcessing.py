0fd8dbca3846bbd3cc9f32013b93be0f9c9f31a9
TsumiDe <m.m.abibulayev@student.khai.edu>
2023-02-26 11:55:32 +0100

    Signal processing, 3

--- SignalProcessing/figures/SignalProcessing.py
+++ SignalProcessing/figures/SignalProcessing.py
@@ -1,5 +1,5 @@
 import numpy
-from scipy import signal, fft
+from scipy import signal
 import matplotlib.pyplot as plt
 
 
@@ -9,8 +9,8 @@ class Main:
         self.n = 500
         self.Fs = 1000
         self.FMax = 3
-        self.Dt = [2, 4, 8, 16]
         self.FFilter = 10
+        self.M = [4, 16, 64, 256]
 
     def main(self):
         randomNormal = numpy.random.normal(0, 10, self.n)
@@ -18,39 +18,39 @@ class Main:
         w = self.FMax / (self.Fs / 2)
         parametersFilter = signal.butter(3, w, "low", output="sos")
         y = signal.sosfiltfilt(parametersFilter, randomNormal)
-        frequencyReadings = fft.fftfreq(self.n, 1 / self.n)
-        frequencyReadingsCenter = fft.fftshift(frequencyReadings)
-        discreteSignals = []
-        discreteSpectrums = []
-        vars2 = []
+        quantizeSignals = []
+        dispersions = []
         SNRList = []
-        for Dt in self.Dt:
-            discreteSignal = numpy.zeros(self.n)
-            for i in range(0, round(self.n / Dt)):
-                discreteSignal[i * Dt] = y[i * Dt]
-            discreteSpectrum = fft.fft(discreteSignal)
-            discreteSpectrum = numpy.abs(fft.fftshift(discreteSpectrum))
-            discreteSignals.append(discreteSignal)
-            discreteSpectrums.append(discreteSpectrum)
-            E1 = discreteSignal - y
-            var1 = numpy.var(y)
-            var2 = numpy.var(E1)
-            vars2.append(var2)
-            SNR = var1 / var2
+        for M in self.M:
+            bits = []
+            delta = (numpy.max(y) - numpy.min(y)) / (M - 1)
+            quantizeSignal = delta * numpy.round(y / delta)
+            quantizeLevels = numpy.arange(numpy.min(quantizeSignal), numpy.max(quantizeSignal) + 1, delta)
+            quantizeBit = numpy.arange(0, M)
+            quantizeBit = [format(bits, '0' + str(int(numpy.log(M) / numpy.log(2))) + 'b') for bits in quantizeBit]
+            quantizeSignals.append(quantizeSignal)
+            quantizeTable = numpy.c_[quantizeLevels[:M], quantizeBit[:M]]
+            self.plotFirst(M, quantizeTable, "Значення сигналу", "Кодова послідовність",
+                           f"Таблиця квантування для {M} рівнів")
+            for signal_value in quantizeSignal:
+                for index, value in enumerate(quantizeLevels[:M]):
+                    if numpy.round(numpy.abs(signal_value - value), 0) == 0:
+                        bits.append(quantizeBit[index])
+                        break
+            bits = [int(item) for item in list(''.join(bits))]
+            self.plotThird(x=numpy.arange(0, len(bits)), y=bits, xLabel="Біти", yLabel="Амплітуда сигналу",
+                           title=f"Кодова послідовність сигналу при кількості рівнів квантування {M}")
+            E = quantizeSignal - y
+            dispersion = numpy.var(E)
+            SNR = numpy.var(y) / dispersion
+            dispersions.append(dispersion)
             SNRList.append(SNR)
-        w = self.FFilter / (self.Fs / 2)
-        parametersFilter = signal.butter(3, w, 'low', output='sos')
-        y = signal.sosfiltfilt(parametersFilter, discreteSignals)
-        self.subplot(x, discreteSignals, "Час (секунди)", "Амплітуда сигналу",
-                     "Сигнал з кроком дисркетизації Dt = (2, 4, 8, 16)")
-        self.subplot(frequencyReadingsCenter, discreteSpectrums, "Частота(Гц)", "Амплітуда спектру",
-                     "Спектри сигналів з кроком дисркетизації Dt = (2, 4, 8, 16)")
-        self.subplot(x, y, "Час (секунди)", "Амплітуда сигналу",
-                     "Відновлені аналогові сигнали з кроком дисркетизації Dt = (2, 4, 8, 16)")
-        self.plot(self.Dt, vars2, "Крок дискретизації", "Дисперсія",
-                  "Залежність дисперсії від кроку дискретизації")
-        self.plot(self.Dt, SNRList, "Крок дискретизації", "ССШ",
-                  "Залежність співвідношення сигнал-шум від кроку дискретизації")
+        self.subplot(x=x, y=quantizeSignals, xLabel="Час (секунди)", yLabel="Амплітуда сигналу",
+                     title="Цифрові сигнали з рівнями квантування (4, 16, 64, 256)")
+        self.plotSecond(x=self.M, y=dispersions, xLabel="Кількість рівнів квантування", yLabel="Дисперсія",
+                        title="Залежність дисперсії від кількості рівнів квантування")
+        self.plotSecond(x=self.M, y=SNRList, xLabel="Кількість рівнів квантування", yLabel="ССШ",
+                        title="Залежність співвідношення сигнал-шум від кількості рівнів квантування")
 
     @staticmethod
     def subplot(x, y, xLabel, yLabel, title):
@@ -70,10 +70,37 @@ class Main:
             print(ex)
 
     @staticmethod
-    def plot(x, y, xLabel, yLabel, title):
+    def plotFirst(M, quantizeTable, colLabelsFirst, colLabelsSecond, title):
+        try:
+            fig, ax = plt.subplots(figsize=(14 / 2.54, M / 2.54))
+            table = ax.table(cellText=quantizeTable, colLabels=[colLabelsFirst, colLabelsSecond],
+                             loc="center")
+            table.set_fontsize(14)
+            table.scale(1, 2)
+            ax.axis('off')
+            fig.savefig(f"{title}.png", dpi=600)
+            plt.show()
+        except Exception as ex:
+            print(ex)
+
+    @staticmethod
+    def plotSecond(x, y, xLabel, yLabel, title):
         try:
             fig, ax = plt.subplots(figsize=(21 / 2.54, 14 / 2.54))
             ax.plot(x, y, linewidth=1)
+            ax.set_xlabel(xLabel)
+            ax.set_ylabel(yLabel)
+            plt.title(title, fontsize=14)
+            fig.savefig(f"{title}.png", dpi=600)
+            plt.show()
+        except Exception as ex:
+            print(ex)
+
+    @staticmethod
+    def plotThird(x, y, xLabel, yLabel, title):
+        try:
+            fig, ax = plt.subplots(figsize=(21 / 2.54, 14 / 2.54))
+            ax.step(x, y, linewidth=0.1)
             ax.set_xlabel(xLabel)
             ax.set_ylabel(yLabel)
             plt.title(title, fontsize=14)
